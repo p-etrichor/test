79eee7bc475b749fa35c0fa3ac432d67350c0340
p-etrichor <4my5wn@gmail.com>
2024-05-24 14:30:49 +0900

    대략 완성, output저장기능 필요

--- get_data_from_web.py
+++ get_data_from_web.py
@@ -18,14 +18,16 @@ def get_date(start, period):
     return end
 
 
-#기간내 최고가와 클로징종가 
+#진입가, 기간내 최고가, 클로징종가 
 def get_numbers_from(start, end, code):
     df = stock.get_market_ohlcv(start, end, code)
+    #df = stock.get_market_ohlcv(start_date, end_date, 377480)
     time.sleep(1)
+    initial = df['시가'].iloc[0]
     highest = max(df['고가'])
     closing = df['종가'].iloc[-1]
     
-    return highest, closing
+    return initial, highest, closing
     
     
 location = 'D:/test/test/'
@@ -37,46 +39,22 @@ raw_data = pd.read_csv(location+filename, dtype=object)
 
 
 #pykrx에서 데이터 조회
-start_date = "2024-01-02"
-end_date = get_date(start_date, 30)
 
 
 for i in range(len(raw_data)):
     #print(i)
     icode = raw_data.loc[i, 'code']
+    start_date = raw_data.loc[i, 'issuedate']
+    end_date = get_date(start_date, 30)
     
-    high, close = get_numbers_from(start_date, end_date, icode)
+    init, high, close = get_numbers_from(start_date, end_date, icode)
 
+    raw_data.loc[i, 'initial'] = init
     raw_data.loc[i, 'highest'] = high
     raw_data.loc[i, 'closing'] = close
     
-    #print(high, close) 
 
+#분석
+raw_data['profit_highest'] = (raw_data['highest'] - raw_data['initial']) / raw_data['initial'] - 0.01
+raw_data['loss_at_close'] = (raw_data['closing'] - raw_data['initial']) / raw_data['initial'] - 0.01
 
-#data.info()
-#data.columns
-#1#번
-#필요한 데이터만 남기기(회사명, 종목코드, 진입날짜, 진입가, 행사가, 어퍼, 최고가, 최고비율)
-data = data.drop(columns = ["결의일", "결의일종가", "현재가", "진입가시총", "탈출가능?", "기간내최저가", "최저비율"])
-#NaN 삭제(행 삭제)
-data = data.dropna()
-
-
-#데이터 cleaning
-data['진입날짜'] = pd.to_datetime(data['진입날짜'])
-data['진입가'] = data['진입가'].astype('int64')
-data['행사가'] = data['행사가'].astype('int64')
-data['기간내최고가'] = data['기간내최고가'].astype('int64')
-
-#데이터 만들기(퍼센트 변환이 귀찮아서 그냥 새로 만듬(이게더 나음))
-data['어퍼룸'] = (data['행사가'] -  data['진입가']) / data['진입가']
-data['최고비율'] = (data['기간내최고가'] -  data['진입가']) / data['진입가']
-
-
-
-
-#count로 세기 NaN은 세지 않는다고 함(size는 NaN도 포함해서 센다고 함)
-#data.count()
-data['회사명'].count()
-
-data[data['최고비율']>=0.1].count()
