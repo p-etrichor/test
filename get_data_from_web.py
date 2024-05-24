ac6e12ac0f15398f075a64d190eed48d1edaec85
p-etrichor <4my5wn@gmail.com>
2024-05-24 15:44:23 +0900

    calculation은 완성, 그런데 analyzing 및 visualizaton 필요

--- get_data_from_web.py
+++ get_data_from_web.py
@@ -28,24 +28,45 @@ def get_numbers_from(start, end, code):
     closing = df['종가'].iloc[-1]
     
     return initial, highest, closing
+
+
+def back_test(raw_data, harvest_profit, period):
+    #대략 백테스트라고...
+    summarine_profit = 0
+    for i in range(len(raw_data)):
+        if raw_data.loc[i, 'upper_room'] > 1:
+            if raw_data.loc[i, 'profit_highest'] > harvest_profit:
+                summarine_profit = summarine_profit + harvest_profit
+            else:
+                summarine_profit = summarine_profit + raw_data.loc[i, 'loss_at_close']
+    
+    print("이익률 " + str(harvest_profit) + ", " + str(period) + "일 적용된 수익률 : " + str(round(summarine_profit, 5) * 100))
+    
+    return summarine_profit
+
     
     
 location = 'D:/test/test/'
 filename = 'test_code_simple.csv'
-
+#harvest_profit = 0.05
+period = 90
+final = pd.DataFrame({'기간':[1,2,3,...,period]})
+#이게 참....period에 맞춰서 한번 뿌리면 harvest_profit에 대한 분석은 쉽다....
+#그러면 period에 의해 가져오는 함수 하나, df를 받아서 분석하는 함수 하나....
+tax = 0.005
 
 #data는 csv로 가져온다 - 종목코드, 진입날짜, 행사가격이 line으로 쌓여있는 csv
 raw_data = pd.read_csv(location+filename, dtype=object)
+raw_data = raw_data.astype({'strike_price':'int32'})
 
 
 #pykrx에서 데이터 조회
-
-
 for i in range(len(raw_data)):
-    #print(i)
     icode = raw_data.loc[i, 'code']
+    print("analyzing " + str(i) + "/" + str(len(raw_data)))
+    
     start_date = raw_data.loc[i, 'issuedate']
-    end_date = get_date(start_date, 30)
+    end_date = get_date(start_date, period)
     
     init, high, close = get_numbers_from(start_date, end_date, icode)
 
@@ -53,8 +74,14 @@ for i in range(len(raw_data)):
     raw_data.loc[i, 'highest'] = high
     raw_data.loc[i, 'closing'] = close
     
+raw_data['upper_room'] = raw_data['strike_price'] / raw_data['initial']
+raw_data['profit_highest'] = (raw_data['highest'] - raw_data['initial']) / raw_data['initial'] - tax
+raw_data['loss_at_close'] = (raw_data['closing'] - raw_data['initial']) / raw_data['initial'] - tax
 
-#분석
-raw_data['profit_highest'] = (raw_data['highest'] - raw_data['initial']) / raw_data['initial'] - 0.01
-raw_data['loss_at_close'] = (raw_data['closing'] - raw_data['initial']) / raw_data['initial'] - 0.01
+#대략 백테스트 수익률 변화해가며
 
+for harvest_profit in range(100):
+    harvest_profit = harvest_profit / 100
+    back_test(raw_data, harvest_profit, period)
+    
+    
